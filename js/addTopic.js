b22b6a418c72dc7db5082b440d0edafab73b71fd
exoceus <exoceus@gmail.com>
2021-08-03 23:52:32 +0000

    add topic logic and conditional routing

---
+++ js/addTopic.js
@@ -0,0 +1,19 @@
+console.log('custom inti')
+
+const topicNameInput = document.querySelector("#topicNameInput")
+const topicDescriptionInput = document.querySelector("#topicDescriptionInput")
+const topicImgInput = document.querySelector("#topicImgInput")
+
+const onAddTopic = () => {
+    firebase.database().ref(`topics`).push({
+        name: topicNameInput.value,
+        description: topicDescriptionInput.value,
+        image: topicImgInput.value
+    })
+        // 3. Clear the form so that we can write a new note
+        .then(() => {
+            topicNameInput.value = "";
+            topicDescriptionInput.value = "";
+            topicImgInput.value = "";
+        });
+}
