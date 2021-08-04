84020c7656fc6a494791eff1f3f97571cb1d229a
Neeha Kurelli <nkurelli@gmail.com>
2021-08-04 00:07:00 +0000

    viewtopics

---
+++ js/viewTopics.js
@@ -0,0 +1,42 @@
+window.onload = event => {
+  // Firebase authentication goes here.
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      // Console log the user to confirm they are logged in
+      console.log("Logged in as: " + user.displayName);
+      const googleUserId = user.uid;
+      getTopics(googleUserId);
+    } else {
+      // If not logged in, navigate back to login page.
+      window.location = "index.html";
+    }
+  });
+};
+
+const getTopics = userId => {
+    console.log("Getnotes called" + userId)
+  const notesRef = firebase.database().ref(`topics/`);
+  notesRef.on("value", snapshot => {
+    const data = snapshot.val();
+    renderDataAsHtml(data);
+  });
+};
+const renderDataAsHtml = data => {
+  let cards = "";
+  for (const noteItem in data) {
+    const note = data[noteItem];
+    cards += createCard(note);
+  }
+  document.querySelector("#app").innerHTML = cards;
+};
+const createCard = note => {
+  return `
+         <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">${note.name}</h6>
+            </div>
+            <div class="card-body">
+                <p>${note.description}</p>
+            </div>
+        </div>`;
+};
